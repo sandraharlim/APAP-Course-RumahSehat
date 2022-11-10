11251a50436e4f92fca789318ebcb0e5f379b6df
Prinata Rakha Santoso <prinatarakha@gmail.com>
2022-11-10 20:36:37 +0700

    initialize project

---
+++ src/main/java/TA_A_ME_61/RumahSehat/RumahSehatApplication.java
@@ -0,0 +1,13 @@
+package TA_A_ME_61.RumahSehat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumahSehatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RumahSehatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TA_A_ME_61/RumahSehat/RumahSehatApplicationTests.java
@@ -0,0 +1,13 @@
+package TA_A_ME_61.RumahSehat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RumahSehatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
