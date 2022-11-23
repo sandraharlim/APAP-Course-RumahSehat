18febc1b7946cab5a04ac604a50cc2d29823d587
Rizky Achmad Fauzan <rizky.achmad91@ui.ac.id>
2022-11-23 18:00:06 +0700

    Fix HTML Footer

---
+++ rumahsehat/src/main/java/TA_A_ME_61/RumahSehat/service/TagihanService.java
@@ -0,0 +1,13 @@
+package TA_A_ME_61.RumahSehat.service;
+
+import TA_A_ME_61.RumahSehat.model.TagihanModel;
+
+import java.util.List;
+
+public interface TagihanService {
+    List<TagihanModel> getListTagihan();
+
+//    List<TagihanModel> getListTagihanByUuid(String uuid);
+
+
+}
