a4c882cbe3d040a33d60985a6d9f9f1269bcbc74
BOY DI CARIO <boy.di@ui.ac.id>
2022-12-22 16:18:39 +0700

    resolve code smells sonarqube

--- rumahsehat/src/main/java/TA_A_ME_61/RumahSehat/service/ChartService.java
+++ rumahsehat/src/main/java/TA_A_ME_61/RumahSehat/service/ChartService.java
@@ -2,8 +2,10 @@ package TA_A_ME_61.RumahSehat.service;
 
 import TA_A_ME_61.RumahSehat.model.AppointmentModel;
 import TA_A_ME_61.RumahSehat.model.DokterModel;
+import TA_A_ME_61.RumahSehat.model.TagihanModel;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ChartService {
     int[] getIncome(List<AppointmentModel> listAppointment);
@@ -11,4 +13,6 @@ public interface ChartService {
     List<DokterModel> getListDokter();
 
     List<DokterModel> getListDokterLineChart(String id1, String id2, String id3, String id4, String id5);
+
+    Map<String, List<Integer>> getIncomeAllDokter(List<DokterModel> listDokter, List<TagihanModel> listTagihan, int tahun);
 }
